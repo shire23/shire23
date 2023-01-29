00ad43038d896935465fe9f8a984e0784c215dc4
Shire <shinta.ramadhanie23@gmail.com>
2023-01-29 14:18:20 +0700

    memambahkan index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, ratione eos. Optio nemo, perspiciatis, sapiente esse minus minima ipsum magnam magni aperiam aut assumenda? Neque consequatur voluptate incidunt obcaecati exercitationem.</p>
+</body>
+</html>
